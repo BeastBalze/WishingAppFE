656d2933fef45054c35400731f57c88d7b016188
BeastBalze <92935529+BeastBalze@users.noreply.github.com>
2023-07-20 13:06:25 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Register from './register';
 import Login from './Login';
 import Home from './Home';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import { useState } from 'react';
 import Forgotpass from './forgotPassword';
 import ResetPassword from './ResetPassword';
@@ -12,14 +12,13 @@ function App() {
   const [logged, setLogged] = useState(cookies.get('token')? true : false);
   const [regist, setRegist] = useState(false);
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={logged ? <Home logged={logged} setLogged={setLogged}/> : (!regist? <Login setLogged={setLogged} setRegist={setRegist}/> : <Register setRegist= {setRegist}/>)} />
         <Route path='/password/forgot' element = {<Forgotpass/>} />
         <Route path='/password/reset/:token' element = {<ResetPassword/>}/>
-
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
