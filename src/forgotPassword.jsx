69b4ac992bccb50e5f588255b4cac3201efb5a67
BeastBalze <92935529+BeastBalze@users.noreply.github.com>
2023-08-27 23:23:49 +0530

    forgotPassword.jsx

--- src/forgotPassword.jsx
+++ src/forgotPassword.jsx
@@ -3,13 +3,16 @@ import './App.css';
 import axios from 'axios';
 const Forgotpass = () => {
     const [email, setEmail] = useState("");
+    const [requesting, setReq] = useState(false);
     const forgotPasswordfunc = async (email) => {
+        setReq(true);
         try {
             const res = await axios.post('https://wishingapp.onrender.com/api/v1/password/forgot', {email})
             if(res.data.success) alert("Check your Mail");
         } catch (error) {
             alert(error.message);
         }
+        setReq(false);
     };
     const subbmitHandeler = (e) => {
         e.preventDefault();
@@ -21,7 +24,7 @@ const Forgotpass = () => {
         <p className='fpp'>Enter your email below to receive a password reset link:</p>
         <form className='fpform' action="#" method="post" onSubmit={subbmitHandeler}>
             <input className='fpinput' type="email" onChange={(e) => setEmail(e.target.value)} required placeholder="Enter your email" />
-            <button className='fpbutton' type="submit">Request Reset Link</button>
+            <button className='fpbutton' type="submit">{requesting?"Requesting..." : "Request Reset Link"}</button>
         </form>
     </div>
     )
