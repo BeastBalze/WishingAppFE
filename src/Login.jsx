66d73b1c2336b63db4f28719990df08ba4f8692c
BeastBalze <92935529+BeastBalze@users.noreply.github.com>
2024-02-20 10:22:39 +0530

    Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -10,14 +10,17 @@ const Login = ({ setLogged, setRegist }) => {
     setLogging(true);
     let res;
       res = await axios.post('https://wishingapp.onrender.com/api/v1/login', { email, password, Credential: true });
+    console.log("1");
       if (res.data.success) {
+        console.log("2");
         const cookies = new Cookies();
         cookies.set("token", res.data.token, {
           expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
         });
         setLogged(true);
       }
-      else {
+      else if (!res.data.success) {
+        console.log("3");
         alert(res.data.message)
       }
     setLogging(false);
